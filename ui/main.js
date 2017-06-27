9685f73230e0a71ee0465038e3b45e72add224cc
KannaDinesh <dineshkannandp@gmail.com>
2017-06-27 08:43:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,14 @@ console.log('Loaded!');
 var element = document.getElementById('change');
 element.innerHTML = 'Keep Calm & Love the Life';
 
+//Move the image on click
+var img = getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,100);
+};
+
